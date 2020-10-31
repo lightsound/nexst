7422a5d9e0eb5428fcece842a82f083a273df682
skoto <0120koto@gmail.com>
2020-10-31 21:14:02 +0900

    :lipstick: Update the UI and style files.

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -10,7 +10,7 @@ export const Header: React.FC = () => (
             <a>Home</a>
           </Link>
         </li>
-        <li>
+        <li style={{ marginTop: 8 }}>
           <Link href="/about">
             <a>About</a>
           </Link>
