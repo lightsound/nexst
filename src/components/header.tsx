0ad3557f9ad4aedba1d055bc43f13a35b270a0c8
skoto <0120koto@gmail.com>
2020-10-31 21:20:52 +0900

    :lipstick: Update the UI and style files.

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,20 +1,22 @@
 import Link from "next/link";
 
+const items = [
+  { href: "/", label: "Home" },
+  { href: "/about", label: "About" },
+];
+
 export const Header: React.FC = () => (
   <header>
     <h1>Title</h1>
     <nav>
       <ul>
-        <li>
-          <Link href="/">
-            <a>Home</a>
-          </Link>
-        </li>
-        <li style={{ marginTop: 8 }}>
-          <Link href="/about">
-            <a>About</a>
-          </Link>
-        </li>
+        {items.map((item) => (
+          <li key={item.href} style={{ display: "contents" }}>
+            <Link href={item.href}>
+              <a style={{ display: "inline-block", padding: 12 }}>{item.label}</a>
+            </Link>
+          </li>
+        ))}
       </ul>
     </nav>
   </header>
